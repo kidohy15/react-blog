ffbc7e9690c6234d691fd45a100329cdf371bd76
kidohy15 <kidohy307@gmail.com>
2024-02-08 02:24:58 +0900

    app.js 코드 정리 & nav 생성

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  let post = "블로그 제목";
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="blog-nav">
+        {/* 스타일 코드 직접 작성 */}
+        <h4 style={{ color: "red", fontSize: "16px" }}>블로그 네비게이션</h4>
+      </div>
+      <h4>{post}</h4>
     </div>
   );
 }
