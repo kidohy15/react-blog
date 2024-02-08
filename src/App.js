3f2048ab131b27166227310224d069c17125d21b
kidohy15 <kidohy307@gmail.com>
2024-02-09 02:48:52 +0900

    블로그 리스트 테스트

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
 import logo from "./logo.svg";
 import "./App.css";
+import { useState } from "react";
+
+const titleData = ["테스트 블로그1", "테스트 블로그2", "테스트 블로그3"];
 
 function App() {
-  let post = "블로그 제목";
+  const [title, setTitle] = useState(titleData);
+
   return (
     <div className="App">
       <div className="blog-nav">
         {/* 스타일 코드 직접 작성 */}
         <h4 style={{ color: "red", fontSize: "16px" }}>블로그 네비게이션</h4>
       </div>
-      <h4>{post}</h4>
+      {title.map((title, index) => (
+        <div className="list" key={index}>
+          <h4>{title}</h4>
+          <p>11</p>
+        </div>
+      ))}
     </div>
   );
 }
